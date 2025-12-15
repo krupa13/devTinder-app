f6588dd8d1ec1086c0ecca762a610697145044cb
Krupa Nandh <kruparaomui@gmail.com>
2025-12-15 23:47:41 +0530

    edit the profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from "react-redux"
+import EditProfile from "./EditProfile"
 
 const Profile = () => {
+
+  const user = useSelector((store) => store.user);
+  
   return (
     <div>
-      Profile Page
+      <EditProfile user={user}/>
     </div>
   )
 }

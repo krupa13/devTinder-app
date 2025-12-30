65b44d0d8512ffda4d3eea0ce6ec9d9d33dbd70e
Krupa Nandh <kruparaomui@gmail.com>
2025-12-30 12:27:40 +0530

    appstore name changed to appStore

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import appStore from "./utils/appstore.js";
+import appStore from "./utils/appStore.js";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
